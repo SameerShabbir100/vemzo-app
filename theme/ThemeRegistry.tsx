e466e1974c5ecbf00216e36d61f8d5299407c6b4
Sheikh M Sameer <112487735+SameerShabbir100@users.noreply.github.com>
2026-08-11 02:18:00 +0500

    chore: downgrade next.js from v16 to v15 for compatibility

--- theme/ThemeRegistry.tsx
+++ theme/ThemeRegistry.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 
